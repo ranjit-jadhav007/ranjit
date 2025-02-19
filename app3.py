051835a8257217b347b124e2dfc1832d5a283cb0
ranjit-jadhav007 <jjranjit80@gmail.com>
2025-02-19 22:03:44 +0530

    initially commit

---
+++ app3.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("This is first App using streamlit")
+
+st.chat_input("enter name")
+
+
+st.audio_input("upload your audio")
+st.file_uploader("upload news article pics")
+st.camera_input("upload image")
